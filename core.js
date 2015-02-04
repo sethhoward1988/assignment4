8f2af1b8aad97989e9db7ea3116a790e488ee8c7
Seth Howard <smiles.seth@gmail.com>
2015-02-04 09:11:25 -0700

    Update core.js
    You are really close and on the right path with this example. Consider instead of trying to add text, you can just add classes to your html elements. For example, the required class could have { border: 1px solid red } which will put a red border around the input element. And you could do the same thing for the validated except make it green. Try adding classes wherever possible. If you do want to add the text saying "required" after a field that is missing something, then consider adding a pseudo :after element to the class instead of actually adding html markup.  See more about the ::after element at https://developer.mozilla.org/en-US/docs/Web/CSS/::after

--- core.js
+++ core.js
@@ -1,21 +1,34 @@
 $(document).ready(function()	{
 	$(":text, #name, #address, #phone, #email").after("<span>*</span>");
-	$("#form").submit(
-		function(event)	{
-			var isValid = true;
-			var name = $("#name").val();
-			if (name.length == 0) {
-				$("#name, #address").next().text("Required");
-				isValid = false;
-			} else {
-				$("#name, #address").next().text("");}
-			if (isValid == false) { event.preventDefault(); }
-	$("#phone").keyup(function() {
-	$("#form").submit(
-		function(event) {
-			var isValid = true;
-			var phone = $("#phone").val();	
-			});
-		});
-	});	
+
+	$("#form").submit(function(event) {
+		var isValid = true;
+		var name = $("#name").val();
+		var phone = $("#phone").val();
+		
+		if (name.length == 0) {
+			// name field is empty
+			$("name").addClass('requred');
+			isValid = false;
+		} else if (phone.length == 0 || phone.length != 10) {
+			// phone is empty or not exactly 10 digits long
+			$("name").addClass('requred');
+			isValid = false;
+		}
+
+		if (isValid == false) { 
+			event.preventDefault(); // Prevent form from submitting
+		}
+	});
+	
+	$("#phone").keyup(function(evt) {
+		var phoneValue = $("#phone").val();
+		if(phoneValue.length != 10){
+			// Phone length is not 10, add an error class and style it
+			$("#phone").addClass('error');
+		} else {
+			// Phone length is exactly 10, add a validated class to let the user know
+			$("#phone").addClass('validated');
+		}
+	});
 });		
